f0f78e449fbf6731bb6a71d7d996736df387f42e
Suemayah Eldursi <suemayaheldursi@sytner.co.uk>
2019-11-23 22:58:55 +0000

    Add Register view model

---
+++ WishList/Models/AccountViewModels.cs/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models.AccountViewModels.cs
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100)]
+        [MinLength(8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
